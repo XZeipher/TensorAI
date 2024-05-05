766fb0eae9b7b295301a494125dcb23ef308d2c4
xzeipher <rishavroy200@gmail.com>
2024-05-05 15:08:07 +0530

    c

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='TensorART',
     author='Alpha Coder',
     author_email='alphacoders@yahoo.com',
-    version='0.1',
+    version='0.0.1',
     long_description=readme,
     long_description_content_type="text/markdown",
     url='https://github.com/XZeipher/TenorAI',
